8d1e19f1a36b09aa0beb4e3205a24af4fb791008
417MahirRahman <35mahirrahman@gmail.com>
2026-05-23 09:29:28 +0600

    server created

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { type Application, type Request, type Response } from "express";
+const app : Application = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req : Request, res : Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
