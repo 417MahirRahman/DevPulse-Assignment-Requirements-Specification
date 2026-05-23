cb4014b007b295dac3110848d276a56b56067df2
417MahirRahman <35mahirrahman@gmail.com>
2026-05-23 10:29:04 +0600

    database connected

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,47 @@
 import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg";
+import config from "./config";
+
 const app : Application = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json());
+
+const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+const initDB = async () => {
+    try{
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(12) NOT NULL,
+                email VARCHAR(20) NOT NULL UNIQUE,
+                password VARCHAR(20) NOT NULL,
+                role VARCHAR(12) DEFAULT 'contributor',
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )`);
+
+            await pool.query(`
+            CREATE TABLE IF NOT EXISTS issues (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(150) NOT NULL,
+                description TEXT NOT NULL CHECK (char_length(description) >= 20),
+                type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+                status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open','in_progress', 'resolved')),
+                reporter_id  INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )`);
+
+        console.log("Database connected successfully!");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    }
+}
+initDB();
 
 app.get("/", (req : Request, res : Response) => {
   res.send("Hello World!");
